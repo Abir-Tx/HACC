9b3d8fd11f3251c5d290463d9f7e7b0914019524
Abir-Tx <28858998+Abir-Tx@users.noreply.github.com>
2020-08-29 16:12:09 +0600

    Hover Effect added on login button

--- HACC_App/src/Home.java
+++ HACC_App/src/Home.java
@@ -1,14 +1,21 @@
-import java.awt.FlowLayout;   
+import java.awt.*;
+import java.awt.FlowLayout;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.UIManager;
 
 public class Home extends JFrame {
     JFrame frame = new JFrame();
     JButton login = new JButton();
     JButton exit = new JButton();
 
-    public Home(){
+    // FlowLayout fl = new FlowLayout(300, 10, 500);
+    public Home() {
         frame.setTitle("Home Automation Control Center");
         frame.setSize(1000, 700);
         frame.setAlwaysOnTop(false);
@@ -16,31 +23,63 @@ public class Home extends JFrame {
         frame.setLocation(150, 100);
         frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
         frame.setVisible(true);
-        frame.setLayout(new FlowLayout());
+        frame.setLayout(new FlowLayout(300, 10, 300));
 
         loginConfig();
+        loginHover();
         exitConfig();
         adder();
     }
 
-    void loginConfig(){
+    void loginConfig() {
         login.setText("Login");
         login.setContentAreaFilled(true);
         login.setEnabled(true);
         login.setFocusPainted(true);
         login.setFocusable(true);
         login.setToolTipText("Press to go to the login windows");
-        // login.setSize(50 , 25);
     }
 
-    void exitConfig(){
+    void exitConfig() {
         exit.setText("Exit");
         exit.setContentAreaFilled(true);
         exit.setEnabled(true);
         exit.setFocusPainted(true);
         exit.setFocusable(true);
         exit.setToolTipText("Press to exit the program");
-        // login.setSize(100  , 50);
+        // login.setSize(100 , 50);
+    }
+
+    void loginHover() {
+        login.addMouseListener(new MouseListener() {
+
+            @Override
+            public void mouseClicked(MouseEvent arg0) {
+
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent arg0) {
+                login.setBackground(Color.BLUE);
+
+            }
+
+            @Override
+            public void mouseExited(MouseEvent arg0) {
+                login.setBackground(Color.WHITE);
+            }
+
+            @Override
+            public void mousePressed(MouseEvent arg0) {
+
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent arg0) {
+
+            }
+            
+        });
     }
 
     void adder(){
