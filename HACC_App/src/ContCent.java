09d18ba0fbb4278a6b0fde03018ac5f66f4a5f47
Abir-Tx <28858998+Abir-Tx@users.noreply.github.com>
2020-09-04 14:14:11 +0600

    Test codes added

--- HACC_App/src/ContCent.java
+++ HACC_App/src/ContCent.java
@@ -18,6 +18,10 @@ public class ContCent {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(600, 300);
 
+        Home.frame.add(contPanelWindow);
+        contPanelWindow.setVisible(true);
+        contPanelWindow.setBackground(Color.BLACK);
+        contPanelWindow.setBounds(0, 0, 1100, 600);
 
         light.setText("Light");
         // light.setOpaque(false);
@@ -36,6 +40,8 @@ public class ContCent {
         contPanelWindow.add(fan);
         contPanelWindow.add(tv);
         contPanelWindow.add(speaker);
+
+        f.add(contPanelWindow);
     }
 
     public static void main(String[] args) {
