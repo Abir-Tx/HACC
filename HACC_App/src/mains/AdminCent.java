c2111af31f45a2e0a6b25de377eead1bb4cbcbcc
Abir-Tx <28858998+Abir-Tx@users.noreply.github.com>
2020-09-14 01:48:49 +0600

    Admin center logging added

--- HACC_App/src/mains/AdminCent.java
+++ HACC_App/src/mains/AdminCent.java
@@ -6,6 +6,10 @@ import javax.swing.JPanel;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 import javax.swing.JButton;
 import utils.Functioner;
@@ -15,6 +19,7 @@ public class AdminCent extends Functioner {
     JPanel headerpanel = new JPanel(null);
     JPanel contPanelWindow = new JPanel(null);
     JButton delLog = new JButton();
+    JButton startLogging = new JButton();
 
     AdminCent() {
         Home.frame.add(contPanelWindow);
@@ -31,11 +36,16 @@ public class AdminCent extends Functioner {
         delLog.setText("Delete Log");
         delLog.setBounds(400, 30, 300, 50);
 
+        startLogging.setText("Start Logging");
+        startLogging.setBounds(400, 100, 300, 50);
+
         //Button Actions
         delLogAction();
+        startLoggingAction();
 
         // Adding to main content panel
         contPanelWindow.add(delLog);
+        contPanelWindow.add(startLogging);
     }
 
     private void delLogAction() {
@@ -63,5 +73,41 @@ public class AdminCent extends Functioner {
             }
             
         });
+    }
+
+    private void startLoggingAction(){
+        startLogging.addActionListener(new ActionListener(){
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int userChoice = JOptionPane.showConfirmDialog(Home.frame, "Are you sure?");
+                if (userChoice == 0 ) {
+                    try {
+                        logCreator();
+                    } catch (IOException ex) {
+                        ex.printStackTrace();
+                    }
+                }
+                else if(userChoice == 2){
+                    System.out.println("Log creation cancelled");
+                }
+                else {
+                    System.out.println("Log creation cancelled");
+                    JOptionPane.showMessageDialog(Home.frame, "Logging turned off");
+                }
+            }
+
+        });
+    }
+    void logCreator() throws IOException {
+        File log = new File("HACC_App/src/Log.txt");
+        BufferedWriter writer = new BufferedWriter(new FileWriter(log));
+
+        writer.write("\t\t\t------LOG SYSTEM OF HACC------");
+        writer.newLine();
+        writer.newLine();
+        writer.write("\t\t\tLogging turned on By Admin on "+date+"\n\n");
+        writer.flush();
+        writer.close();
     }
 }
