1e00b49c3f85977a40f6facd2e860cff6a828eb2
Abir-Tx <28858998+Abir-Tx@users.noreply.github.com>
2020-09-14 00:58:34 +0600

    Overrided the authenticator method for admin login

--- HACC_App/src/mains/Admin.java
+++ HACC_App/src/mains/Admin.java
@@ -1,5 +1,97 @@
 package mains;
 
-public class Admin {
-    
+import java.awt.Font;
+import java.util.ArrayList;
+
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import java.awt.event.ActionListener;
+
+
+public class Admin extends Login {
+    Admin(){};   //Def cons
+
+    @Override
+    void authenticator(){
+        JLabel errorMsg = new JLabel();
+        errorMsg.setFont(new Font("Times New Roman", Font.PLAIN, 10));
+        errorMsg.setBounds(100, 190, 250, 20);
+        loginPanel.add(errorMsg);
+
+        final String username, password;
+        username = field1.getText().toString();
+        password = field2.getText().toString();
+
+        // Stored Password & username
+        final ArrayList<String> registeredUsernames = new ArrayList<String>();
+        registeredUsernames.add("Abir");
+        final ArrayList<String> registeredPasswords = new ArrayList<String>();
+        registeredPasswords.add("1234");
+
+        for (int i = 0; i < registeredUsernames.toArray().length; i++) {
+            if (username.equals(registeredUsernames.get(i)) && password.equals(registeredPasswords.get(i))) {
+                welcomeMsg();
+                Home.frame.getContentPane().removeAll();
+                Home.frame.repaint();
+                new About(); // creating the control center
+                Home.frame.validate();
+                break;
+            } else if (!(username.equals(registeredUsernames.get(i)) && password.equals(registeredPasswords.get(i)))) {
+                if (username.equals(registeredUsernames.get(i)) && !password.equals(registeredPasswords.get(i))) {
+                    JOptionPane.showMessageDialog(Home.frame, "Wrong Password ! Please recheck your Password");
+                } else if (!username.equals(registeredUsernames.get(i))
+                        && password.equals(registeredPasswords.get(i))) {
+                    JOptionPane.showMessageDialog(Home.frame, "Wrong Username ! Please recheck your Username");
+                } else if (!(username.equals(registeredUsernames.get(i)))
+                        && !(password.equals(registeredPasswords.get(i)))) {
+                    if (username.length() == 0 || password.length() == 0) {
+                        if (username.length() == 0 && password.length() == 0) {
+                            errorMsg.setText("Both Fields cannot be empty");
+                            errorMsg.setForeground(java.awt.Color.RED);
+                            errorMsg.setToolTipText("Fields should be filled with correct infos");
+                            loginPanel.repaint();
+
+                            int delay = 4000; // milliseconds
+                            ActionListener taskPerformer = new ActionListener() {
+                                @Override
+                                public void actionPerformed(java.awt.event.ActionEvent evt) {
+                                    errorMsg.setVisible(false);
+                                }
+                            };
+                            new javax.swing.Timer(delay, taskPerformer).start();
+                        } else {
+                            errorMsg.setText("Any of the Fields cannot be empty");
+                            errorMsg.setForeground(java.awt.Color.RED);
+                            errorMsg.setToolTipText("Fields should be filled with correct infos");
+                            loginPanel.repaint();
+
+                            int delay = 4000; // milliseconds
+                            ActionListener taskPerformer = new ActionListener() {
+                                @Override
+                                public void actionPerformed(java.awt.event.ActionEvent evt) {
+                                    errorMsg.setVisible(false);
+                                }
+                            };
+                            new javax.swing.Timer(delay, taskPerformer).start();
+                        }
+                    } else {
+                        errorMsg.setText("Invalid Authentication ! Try Again");
+                        errorMsg.setForeground(java.awt.Color.RED);
+                        errorMsg.setToolTipText(
+                                "This is an error message. Please input correct username and password to login");
+                        loginPanel.repaint();
+
+                        int delay = 4000; // milliseconds
+                        ActionListener taskPerformer = new ActionListener() {
+                            @Override
+                            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                                errorMsg.setVisible(false);
+                            }
+                        };
+                        new javax.swing.Timer(delay, taskPerformer).start();
+                    }
+                }
+            }
+        }
+    }
 }
