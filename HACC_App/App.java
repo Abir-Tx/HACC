9687ed78e5654154ea0eb3c0ba62e107940f05d6
Abir-Tx <28858998+Abir-Tx@users.noreply.github>
2020-09-21 12:58:01 +0600

    log not found fixed

--- HACC_App/App.java
+++ HACC_App/App.java
@@ -2,6 +2,7 @@ import res.mains.Home;
 
 public class App {
     public static void main(String[] args) throws Exception {
+        System.out.println("______________HACC APP__________________\n\n");
         System.out.print("Starting the App");
         lib.DesignUtils.consoleAnim(10,".",80);
         new Home();
