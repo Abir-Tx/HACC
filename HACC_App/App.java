c3740c3bedb1894629860177f8b02846e631883e
Abir-Tx <28858998+Abir-Tx@users.noreply.github.com>
2020-09-22 22:02:05 +0600

    tab space add

--- HACC_App/App.java
+++ HACC_App/App.java
@@ -3,7 +3,7 @@ import res.mains.Home;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("______________HACC APP__________________\n\n");
+        System.out.println("\t\t\t\t______________HACC APP__________________\n\n");
         System.out.print("Starting the App");
         lib.DesignUtils.consoleAnim(10,".",50);
         new Home();
